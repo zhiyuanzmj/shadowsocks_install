f732107c6ddf9aad9cfc4af2aac2da2362e99491
zmj <zmj@zmjdeMacBook-Pro.local>
2019-06-20 22:08:06 +0800

    zmj2

--- shadowsocksR.sh
+++ shadowsocksR.sh
@@ -20,7 +20,8 @@ echo "#############################################################"
 echo
 
 libsodium_file="libsodium-1.0.17"
-libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz"
+#libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz"
+libsodium_url="https://www.allssr.com/releases/libsodium-1.0.17.tar.gz"
 shadowsocks_r_file="shadowsocksr-3.2.2"
 shadowsocks_r_url="https://github.com/shadowsocksrr/shadowsocksr/archive/3.2.2.tar.gz"
 
